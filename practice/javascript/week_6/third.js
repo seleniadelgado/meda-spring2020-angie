443ab35b9ae59940f2b47c0c9fca095c7f1888ad
seleniadelgado <angiedelgado08@gmail.com>
2020-02-25 12:39:25 -0800

    javascript practice

--- practice/javascript/week_6/third.js
+++ practice/javascript/week_6/third.js
@@ -2,10 +2,18 @@
 var sum = 1000 + 50;
 var product = 50 * 50 * 7;
 var total = sum + product;
-console.log(total);
+// console.log(total);
 
+//looked at the builtin library
 var radius = 87;
 var diameterOfCircle = 2 * radius;
+//used math PI
 var perimeterOfCircle = Math.PI * diameterOfCircle;
 
-console.log("The diameter of the circle with a radius of "+radius + " is " + diameterOfCircle + " the perimeter of the circle is " + perimeterOfCircle);
+// console.log("The diameter of the circle with a radius of "+radius + " is " + diameterOfCircle + " the perimeter of the circle is " + perimeterOfCircle);
+
+console.log("round 4.5 to nearest number " + Math.round(4.5));
+console.log("round up 45.1 is " + Math.ceil(-.01));
+console.log("round down 45.9 is " + Math.floor(45.9));
+
+var someNumber = Math.random();
