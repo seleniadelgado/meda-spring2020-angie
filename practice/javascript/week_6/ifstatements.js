221cdf0106c0638af83e9fd2f93eb69c53e3de94
seleniadelgado <angiedelgado08@gmail.com>
2020-02-28 11:07:01 -0800

    all changes using if statements

--- practice/javascript/week_6/ifstatements.js
+++ practice/javascript/week_6/ifstatements.js
@@ -1,5 +1,54 @@
 //If-else statements
-if (false){
-    console.log("This code runs if condition is true.");
+// if (false){
+//     console.log("This code runs if condition is true.");
+// }
+//console.log("End of script");
+// the = sign is used for assigning values to vars.
+// the == sign means equal to.
+// the >= greater than.
+// the <= less than.
+// != NOT.
+
+// 
+// var test = "1000";
+// if (test == 1000){
+//     console.log(1);
+// }
+// type coercion - forces a data type to be another data type.
+// If and Else parts
+//if value is not assigned anything, it is then called undefined.
+// falsy - becomes false in a condition, and truthy becomes true.
+// zero, null, 0, "", NaN
+// truthy - becomes truthy in a condition.
+// false
+
+// var sayhello;
+
+// if (false) {
+//     console.log("hello")
+// } else {
+//     console.log("goodbye")
+// }
+// var z = '0'
+// if ('0' == 0) {
+//     console.log("hi");
+// }
+
+// var firstBool = true;
+// var secondBool = false;
+
+// if (firstBool || secondBool) {
+//     console.log('hello');
+// }
+
+// var level = "25";
+
+// if (level > 0 && level < 18) {
+//     console.log("You are child");
+// }
+// else if (level >= 18 && level < 112) {
+//     console.log("You're old");
+// }
+if (true && false || true || false && true) {
+    console.log("true");
 }
-console.log("End of script");
